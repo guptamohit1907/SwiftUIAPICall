9aaa84c29e156054ea15c348db268ea5770ed3bc
Mohit Gupta <Mohit.Gupta14@ibm.com>
2023-06-26 22:11:33 +0530

    Initial Project Setup

---
+++ SwiftUIAPICall/SwiftUIAPICall/SwiftUIAPICallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPICallApp.swift
+//  SwiftUIAPICall
+//
+//  Created by Mohit Gupta on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPICallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
